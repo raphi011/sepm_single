337185d99e4cb187763ca25f2054431d21b41328
Raphael Gruber <raphi011@gmail.com>
2014-10-14 13:28:48 +0000

    * Added support for maven * new h2 connection ( with correct singleton) * added tests for DAOCustomer
    git-svn-id: https://sat.inso.tuwien.ac.at/repositories/svn/sepm_ws14_0828630@5 7194f472-34a2-446f-a650-32dea9ab4b9d

--- src/sepm/ws14/e0828630/dao/DAOException.java
+++
@@ -1,8 +0,0 @@
-package sepm.ws14.e0828630.dao;
-
-public class DAOException extends Exception {
-
-    public DAOException() { super(); }
-
-    public DAOException(String text) { super(text); }
-}
--- src/sepm/ws14/e0828630/gui/Controller.java
+++
@@ -1,4 +0,0 @@
-package sepm.ws14.e0828630.gui;
-
-public class Controller {
-}
